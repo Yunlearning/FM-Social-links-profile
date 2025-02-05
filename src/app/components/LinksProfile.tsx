15cbeda26e961019aeebfd5bcd906491382d4e56
Yunlearning <42810537+Yunlearning@users.noreply.github.com>
2025-02-05 14:54:19 +0800

    Initial commit

---
+++ src/app/components/LinksProfile.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image"
+import userAvastar from "@/app/assets/images/avatar-jessica.jpeg"
+import { StaticImageData } from "next/image";
+
+interface UserProfile {
+    name: string;
+    avatar: StaticImageData;
+    location: string;
+    description: string;
+}
+
+const userProfile: UserProfile = {
+    name: "Jessica Randall",
+    avatar: userAvastar,
+    location: "London, United Kingdom",
+    description: "Front-end developer and avid reader.",
+}
+
+const socialLinks: string[] = [
+    "GitHub",
+    "Frontend Mentor",
+    "LinkedIn",
+    "Twitter",
+    "Instagram",
+]
+
+export default function LinksProfile() {
+    return (
+        <div className="bg-profile-grey-800 flex flex-col justify-center items-center p-8 gap-4 w-32 mobile:w-80 desktop:w-80 rounded-custom-lg">
+            <Image src={userProfile.avatar} alt={userProfile.name} className="w-24 h-24 rounded-full" width={24} height={24} />
+            <section className="space-y-2 text-center">
+                <h1 className="text-2xl font-bold ">{userProfile.name}</h1>
+                <address className="text-sm text-profile-green font-bold not-italic">{userProfile.location}</address>
+            </section>
+
+            <p className="text-sm py-2">&quot;{userProfile.description}&quot;</p>
+
+            {
+                socialLinks.map((link, index) => (
+                    <div key={index} className="bg-profile-grey-700 w-full flex justify-center items-center h-12 text-sm font-bold rounded-custom-lg cursor-pointer active:bg-profile-green active:text-profile-grey-700">
+                        {link}
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            screens: {
+                mobile: '375px',
+                desktop: '1440px',
+            },
+            borderRadius: {
+                'custom-lg': '10px',
+                'custom-xl': '20px',
+            },
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+                'profile-green': 'hsl(75, 94%, 57%)',
+                'profile-grey-700': 'hsl(0, 0%, 20%)',
+                'profile-grey-800': 'hsl(0, 0%, 12%)',
+                'profile-grey-900': 'hsl(0, 0%, 8%)',
+            },
+            backgroundColor: {
+                'profile-green': 'hsl(75, 94%, 57%)',
+            },
+            fontFamily: {
+                inter: ['Inter', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
